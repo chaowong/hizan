575d3dfd031c983c748882e2426bcad61b88af3c
Alex <alex@alexdemac-mini.local>
2015-11-01 00:18:01 +0800

    init files

---
+++ hizan/hizan/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  hizan
+//
+//  Created by Alex on 15/11/1.
+//  Copyright © 2015年 Alex. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
